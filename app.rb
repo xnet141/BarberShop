eae374a098cf43ae97f4da91683b8fd78941b7ba
Alex Erofeev <xnet141@gmail.com>
2024-06-05 12:29:17 +0300

    Fix undefined local variable error

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def get_db
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
+end
+
 configure do
 	@db = SQLite3::Database.new 'barbershop.db'
 	@db.execute 'CREATE TABLE IF NOT EXISTS 
@@ -145,9 +151,5 @@ post '/login' do
 	end
 end
 
-def get_db
-	db = SQLite3::Database.new 'barbershop.db'
-	db.results_as_hash = true
-	return db
-end
+
 
